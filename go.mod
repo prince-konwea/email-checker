7e4760f9164fa908d3a62dc1c19a9ce36ec10f2c
prince-konwea <kokodecontee123@gmail.com>
2023-03-24 17:27:56 +0100

    complete function for retrieving dmarc spf and mx records

---
+++ go.mod
@@ -0,0 +1,3 @@
+module example.com/email.checker
+
+go 1.20
--- main.go
+++ main.go
@@ -5,57 +5,59 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"strings"
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.stdin)
+	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Printf("domain, hasMX, hasSPF, spfRecord, hasDMARC, dmarcRecord\n")
 
-	for scanner.scan() {
+	for scanner.Scan() {
 		checkDomain(scanner.Text())
 
-		if err := scanner.err(); err != nil {
-			log.Fatal("Error: could not readfrom input: %v", err)
+		if err := scanner.Err(); err != nil {
+			log.Fatal("Error: could not readfrom input: %v\n", err)
 		}
 	}
 }
 
 func checkDomain(domain string) {
-   var hasMX, hasSPF, hasDMARC, bool
-   var hasSPF, dmarcRecord string
+	var hasMX, hasSPF, hasDMARC bool
+	var spfRecord, dmarcRecord string
 
-   mxRecords, err := net.LookupMX(domain)
+	mxRecords, err := net.LookupMX(domain)
 
-   if err != nil{
-	 log.Printf("Error: %v", err)
-   }
-   if len(mxRecords) > 0 {
-	hasMX = true
-   }
-   txtRecords, err := net.Lookup.TXT(domain)
-   if err != nil {
-	log.Printf("Error: %v", err)
-   }
-
-   for _, record := range txtRecord {
-	if strings.HasPrefix(record, "v=spf1"){
-		hasSPF =true
-		spfRecord = record
-		break
+	if err != nil {
+		log.Printf("Error: %v", err)
 	}
-   }
+	if len(mxRecords) > 0 {
+		hasMX = true
+	}
+	txtRecords, err := net.LookupTXT(domain)
+	if err != nil {
+		log.Printf("Error: %v", err)
+	}
 
-   dmarcRecords, err := net.LookupTXT("_dmarc." + domain)
-   if err != nil {
-	log.Printf("Error: %v\n", err)
-   }
+	for _, record := range txtRecords {
+		if strings.HasPrefix(record, "v=spf1") {
+			hasSPF = true
+			spfRecord = record
+			break
+		}
+	}
 
-   for _, record := range dmarcRecords {
-     if strings.HasPrefix(record , "v=DMARC1"){
-		hasDMARC = true
-		dmarcRecord = record
-		break
-	 }
-   }
-   fmt.Printf("%v", domain, hasMX, hasSPF, spfRecord, hasDMARC, dmarcRecord\n)
-} 
+	dmarcRecords, err := net.LookupTXT("_dmarc." + domain)
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+
+	for _, record := range dmarcRecords {
+		if strings.HasPrefix(record, "v=DMARC1") {
+			hasDMARC = true
+			dmarcRecord = record
+			break
+		}
+	}
+	fmt.Printf("%v, %v, %v, %v, %v, %v,", domain, hasMX, hasSPF, spfRecord, hasDMARC, dmarcRecord)
+}
